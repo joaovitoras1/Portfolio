13f3d010d82d9aaf1c2c267f68055946c759caff
João Vitor <jvaee742@gmail.com>
2026-07-25 22:58:02 -0300

    style: formatação

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -2,10 +2,10 @@ import foto from '../../assets/foto.jpg'
 
 function Hero() {
     return (
-        <div className="flex flex-col items-center">
-            <img src={foto} alt="foto-de-perfil-utilizando-camisa-social-branca" />
-            <p>Hello World! Meu nome é <span>João Vitor</span> e sou</p>
-            <h1>Desenvolvedor Front-end</h1>
+        <div className="flex flex-col gap-1 p-2  items-center leading-none">
+            <img src={foto} alt="foto-de-perfil-utilizando-camisa-social-branca" className="size-30 rounded-[50%] mb-5 " />
+            <p className="text-[16px]">Hello World! Meu nome é <span className="" >João Vitor</span> e sou</p>
+            <h1 className="text-[38px]">Desenvolvedor Front-end</h1>
         </div>
     )
 }
