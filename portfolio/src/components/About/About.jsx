84c57eb4c643fec3867c8178d69021e3f367cdf5
João Vitor <jvaee742@gmail.com>
2026-07-27 16:54:51 -0300

    Create About.jsx
    Criando o componente About

---
+++ portfolio/src/components/About/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <div>
+            <h2>Sobre Mim</h2>
+            <p>Olá, me chamo João Vitor, estudante autodidata de desenvolvimento web front-end.</p>
+            <p>Aqui irei mostrar minhas habilidades e projetos práticos que fiz durante minha jornada de aprendizado.</p>
+            <ul>
+                <li>Habilidades em HTML, CSS e Javascript, com foco em criar designs responsivos e acessíveis.</li>
+                <li>Proficiente no uso de frameworks modernos de front-end, como o React e o TailwindCSS, utilizados para construir e estilizar interfaces de usuário dinâmicas e interativas</li>
+                <li>Sempre em busca de conhecimento e habilidades que possam ser úteis para o campo de desenvolvimento web que está em constante evolução.</li>
+            </ul>
+        </div>
+    )
+}
