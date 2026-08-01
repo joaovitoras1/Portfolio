318f1314bfa95a42bf0fd203a54d9dfeb38508b3
João Vitor <jvaee742@gmail.com>
2026-07-31 21:49:58 -0300

    Update App.jsx
    Docs: Adicionando imports e components react ao function

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import Menu from './components/Nav-bar/Menu.jsx'
 import Hero from './components/Hero/Hero.jsx'
+import About from './components/About/About.jsx'
+import Technologies from './components/Technologies/Technologies.jsx'
+import Projects from './components/Projects/Projects.jsx'
 
 function App() {
   return (
-    <>
-      <Hero />
-    </>
+    <div className="bg-[url('/bg-mobile.jpg')] md:bg-[url('/bg-desktop.jpg')] bg-cover bg-center w-screen h-screen">
+      <Menu />
+      <div>
+        <Hero />
+        <About />
+        <Technologies />
+        <Projects />
+      </div>
+    </div>
   )
 }
 
