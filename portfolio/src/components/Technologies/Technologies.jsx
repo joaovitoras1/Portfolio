2c32e272a0c2048a265632e204a827b69b7ef66d
João Vitor <jvaee742@gmail.com>
2026-07-31 21:47:18 -0300

    Component
    Component: ícones de tecnologias/ferramentas de desenvolvimento

---
+++ portfolio/src/components/Technologies/Technologies.jsx
@@ -0,0 +1,26 @@
+import './Technologies.css'
+import reactIcon from '../../assets/icons/React.svg'
+import tailwindIcon from '../../assets/icons/Tailwind.svg'
+import javascriptIcon from '../../assets/icons/JavaScript.svg'
+import cssIcon from '../../assets/icons/CSS.svg'
+import htmlIcon from '../../assets/icons/HTML.svg'
+import gitIcon from '../../assets/icons/Git.svg'
+
+const icons = [
+  { nome: "React", icone: reactIcon },
+  { nome: "Tailwind", icone: tailwindIcon },
+  { nome: "JavaScript", icone: javascriptIcon },
+  { nome: "CSS", icone: cssIcon },
+  { nome: "HTML", icone: htmlIcon },
+  { nome: "Git", icone: gitIcon },
+]
+
+function Technologies() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Technologies
