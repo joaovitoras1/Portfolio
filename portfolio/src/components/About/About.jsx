c9d97135dcd6153022be349edf361740a1276a2f
João Vitor <jvaee742@gmail.com>
2026-08-03 17:15:09 -0300

    Update About.jsx
    Estilização

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -1,10 +1,9 @@
 function About() {
     return (
-        <div>
-            <h2>Sobre Mim</h2>
-            <p>Olá, me chamo João Vitor, estudante autodidata de desenvolvimento web front-end.</p>
-            <p>Aqui irei mostrar minhas habilidades e projetos práticos que fiz durante minha jornada de aprendizado.</p>
-            <ul>
+        <div className="p-10 place-items-center">
+            <h2 className="pb-5 text-[20px] text-center">Sobre Mim</h2>
+            <p>Olá, me chamo João Vitor, estudante autodidata de desenvolvimento web front-end. Aqui irei mostrar minhas habilidades e projetos práticos que fiz durante minha jornada de aprendizado.</p>
+            <ul className="flex flex-col gap-2 p-5 list-disc">
                 <li>Habilidades em HTML, CSS e Javascript, com foco em criar designs responsivos e acessíveis.</li>
                 <li>Proficiente no uso de frameworks modernos de front-end, como o React e o TailwindCSS, utilizados para construir e estilizar interfaces de usuário dinâmicas e interativas</li>
                 <li>Sempre em busca de conhecimento e habilidades que possam ser úteis para o campo de desenvolvimento web que está em constante evolução.</li>
