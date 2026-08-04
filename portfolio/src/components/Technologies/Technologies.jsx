39200b6c77a3bda477fc01d6a0b01c1e85a956f0
João Vitor <jvaee742@gmail.com>
2026-08-04 16:24:14 -0300

    feat: adiciona componente Technologies com listagem de ícones

--- portfolio/src/components/Technologies/Technologies.jsx
+++ portfolio/src/components/Technologies/Technologies.jsx
@@ -17,8 +17,13 @@ const icons = [
 
 function Technologies() {
     return (
-        <div>
-            
+        <div className="flex flex-col items-center">
+            <h2 className="pb-5 text-[20px] font-open-sans">Tecnologias</h2>
+            <div className="p-5 flex flex-wrap gap-5 md:flex-nowrap flex-row justify-center">
+                {icons.map((item, index) => (
+                    <img src={item.icone} alt={item.nome} key={index} className="size-10" />
+                ))}
+            </div>
         </div>
     )
 }
