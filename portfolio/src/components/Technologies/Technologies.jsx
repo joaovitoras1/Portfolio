64638426ad6d92151d2a7a825c0d421ec771da75
João Vitor <jvaee742@gmail.com>
2026-08-05 11:05:01 -0300

    refactor: reorganizando

--- portfolio/src/components/Technologies/Technologies.jsx
+++ portfolio/src/components/Technologies/Technologies.jsx
@@ -1,18 +1,18 @@
 import './Technologies.css'
+import htmlIcon from '../../assets/icons/HTML.svg'
+import cssIcon from '../../assets/icons/CSS.svg'
+import javascriptIcon from '../../assets/icons/JavaScript.svg'
+import gitIcon from '../../assets/icons/Git.svg'
 import reactIcon from '../../assets/icons/React.svg'
 import tailwindIcon from '../../assets/icons/Tailwind.svg'
-import javascriptIcon from '../../assets/icons/JavaScript.svg'
-import cssIcon from '../../assets/icons/CSS.svg'
-import htmlIcon from '../../assets/icons/HTML.svg'
-import gitIcon from '../../assets/icons/Git.svg'
 
 const icons = [
-  { nome: "React", icone: reactIcon },
-  { nome: "Tailwind", icone: tailwindIcon },
-  { nome: "JavaScript", icone: javascriptIcon },
-  { nome: "CSS", icone: cssIcon },
-  { nome: "HTML", icone: htmlIcon },
-  { nome: "Git", icone: gitIcon },
+    { nome: "HTML", icone: htmlIcon },
+    { nome: "CSS", icone: cssIcon },
+    { nome: "JavaScript", icone: javascriptIcon },
+    { nome: "Git", icone: gitIcon },
+    { nome: "React", icone: reactIcon },
+    { nome: "Tailwind", icone: tailwindIcon },
 ]
 
 function Technologies() {
