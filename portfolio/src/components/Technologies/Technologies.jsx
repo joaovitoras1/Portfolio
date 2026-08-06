7812cf79df66e8a7f5134fceaa1308d2c0a9ac87
João Vitor <jvaee742@gmail.com>
2026-08-05 22:24:11 -0300

    feat: Adicionando animação com delay nos ícones

--- portfolio/src/components/Technologies/Technologies.jsx
+++ portfolio/src/components/Technologies/Technologies.jsx
@@ -21,7 +21,7 @@ function Technologies() {
             <h2 className="pb-5 text-[20px] font-open-sans">Tecnologias</h2>
             <div className="p-5 flex flex-wrap gap-5 md:flex-nowrap flex-row justify-center">
                 {icons.map((item, index) => (
-                    <img src={item.icone} alt={item.nome} key={index} className="size-10" />
+                    <img src={item.icone} alt={item.nome} key={index} className="size-10 animate-float" style={{animationDelay: index % 2 === 0 ? '0s' : '1.5s'}} />
                 ))}
             </div>
         </div>
