9b1365a3ac59178491f7745e00ed6a52d3da09ac
João Vitor <jvaee742@gmail.com>
2026-08-18 16:51:16 -0300

    feat: esqueleto de lógica de dados dos projetos

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,14 @@
+import projectImage from '../assets/projectsImg/'
+import projectImage from '../assets/projectsImg/'
+import projectImage from '../assets/projectsImg/'
+import projectImage from '../assets/projectsImg/'
+import projectImage from '../assets/projectsImg/'
+import projectImage from '../assets/projectsImg/'
+
+export const projects = [
+    { image: projectImage, title: "", description: "", link: "" },
+    { image: projectImage, title: "", description: "", link: "" },
+    { image: projectImage, title: "", description: "", link: "" },
+    { image: projectImage, title: "", description: "", link: "" },
+    { image: projectImage, title: "", description: "", link: "" },
+]
