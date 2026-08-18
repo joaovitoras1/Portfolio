d254888ecfc806b45251720295451819d9f8cb96
João Vitor <jvaee742@gmail.com>
2026-08-18 16:44:26 -0300

    feat: adiciona componente visual do menu

---
+++ portfolio/src/components/Nav-bar/Menu.jsx
@@ -0,0 +1,13 @@
+function Menu() {
+    return (
+        <div>
+            <ul className="flex flex-row gap-3 p-2 justify-center font-open-sans">
+                <li><a href="">Sobre Mim</a></li>
+                <li><a href="">Tecnologias</a></li>
+                <li><a href="">Projetos</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
